1a75d2eb7d2c381bb6f8433ffca245817f55f5f4
faradilahputridy <faradilahputri2134@gmail.com>
2020-08-10 23:07:08 +0700

    Mencoba push projek dengan menambahkan note pada activity

--- app/src/main/java/com/androfara/bwamovapp/SplashScreenActivity.kt
+++ app/src/main/java/com/androfara/bwamovapp/SplashScreenActivity.kt
@@ -6,6 +6,12 @@ import android.os.Bundle
 import android.os.Handler
 import com.androfara.bwamovapp.onboarding.OnboardingOneActivity
 
+/*
+    Ini adalah activity pertama yang di run
+    Tidak ada fitur spesial disini
+    Hanya melekukan pending beberapa detik saja
+ */
+
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
